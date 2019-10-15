61bcd8a08296f035e610d2408e0b6aef0d2c9708
Izuking <matthiasogbodo@gmail.com>
2019-10-15 13:08:10 +0100

    initial commit

---
+++ src/test/java/com/paga/librarycatalogue/LibraryCatalogueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.paga.librarycatalogue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibraryCatalogueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
