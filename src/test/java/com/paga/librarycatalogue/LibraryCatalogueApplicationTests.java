c327012aef6bbb3a39b09a5ed377f4bf69898a04
Izuking <matthiasogbodo@gmail.com>
2019-10-15 19:17:55 +0100

    reorganised test structure

--- src/test/java/com/paga/librarycatalogue/LibraryCatalogueApplicationTests.java
+++ src/test/java/com/paga/librarycatalogue/LibraryCatalogueApplicationTests.java
@@ -11,6 +11,7 @@ import com.paga.librarycatalogue.dao.FakeCatalogueDataAccess;
 import com.paga.librarycatalogue.model.Catalogue;
 import com.paga.librarycatalogue.service.CatalogueService;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,16 +29,27 @@ public class LibraryCatalogueApplicationTests {
 	@MockBean
 	private FakeCatalogueDataAccess repository;
 
+	private Catalogue catalogue1;
+	private Catalogue catalogue2;
+	private Catalogue catalogue3;
+	private Catalogue catalogue4;
+
+	@Before
+	public void initializer() {
+		// Create some dialogue objects
+		catalogue1 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD1", "Joys of Cooking",
+				"Simon Clark", "1995", "Cookbook");
+		catalogue2 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD2", "Black Gold",
+				"Chima Adize", "2013", "Action Adventure");
+		catalogue3 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD3",
+				"Natural Consumption", "Isaac Network", "1899", "Science");
+		catalogue4 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD1", "I, Zombie",
+				"Israel Izamov", "2011", "Science Fiction");
+
+	}
+
 	@Test
 	public void getAllCatalogueTest() {
-		// Create some dialogue objects
-		Catalogue catalogue1 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD1",
-				"Joys of Cooking", "Simon Clark", "1995", "Cookbook");
-		Catalogue catalogue2 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD2",
-				"Black Gold", "Chima Adize", "2013", "Action Adventure");
-		Catalogue catalogue3 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD3",
-				"Natural Consumption", "Isaac Network", "1899", "Science");
-
 		// using Mockito method to Mock the data to be returned here
 		when(repository.selectAllCatalogue())
 				.thenReturn(Stream.of(catalogue1, catalogue2, catalogue3).collect(Collectors.toList()));
@@ -48,26 +60,14 @@ public class LibraryCatalogueApplicationTests {
 	@Test
 	public void addNewCatalogueTest() {
 
-		Catalogue newCatalogue = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD1",
-				"I, Zombie", "Israel Izamov", "2011", "Science Fiction");
-
 		// using Mockito method to Mock the data to be returned here
-		when(repository.addCatalogue(newCatalogue)).thenReturn(newCatalogue);
+		when(repository.addCatalogue(catalogue4)).thenReturn(catalogue4);
 
-		assertEquals(newCatalogue, service.addCatalogue(newCatalogue));
+		assertEquals(catalogue4, service.addCatalogue(catalogue4));
 	}
 
 	@Test
 	public void filterCatalogueTest() {
-		// Create some dialogue objects
-		Catalogue catalogue1 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD1",
-				"Joys of Cooking", "Simon Clark", "1995", "Cookbook");
-		Catalogue catalogue2 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD2",
-				"Black Gold", "Chima Adize", "2013", "Action Adventure");
-		Catalogue catalogue3 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD3",
-				"Natural Consumption", "Isaac Network", "1899", "Science");
-		Catalogue catalogue4 = new Catalogue(UUID.fromString("f44af12e-322d-448a-8fd2-6fca1c3d6814"), "LD1",
-				"I, Zombie", "Israel Izamov", "2011", "Science Fiction");
 
 		repository.addCatalogue(catalogue1);
 		repository.addCatalogue(catalogue2);
