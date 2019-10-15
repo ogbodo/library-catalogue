2425f8dc02cfbf009a12a88998c98205b8f90c61
Izuking <matthiasogbodo@gmail.com>
2019-10-15 13:09:09 +0100

    initial commit

---
+++ src/main/java/com/paga/librarycatalogue/LibraryCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.paga.librarycatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCatalogueApplication.class, args);
+	}
+
+}
