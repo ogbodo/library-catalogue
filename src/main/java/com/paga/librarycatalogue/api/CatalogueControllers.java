9c980c19298fd7e5815d171dd928811fb0c4c8c2
Izuking <matthiasogbodo@gmail.com>
2019-10-15 14:03:12 +0100

    Injected some RESTful dependencies  into catalog ue controller class

--- src/main/java/com/paga/librarycatalogue/api/CatalogueControllers.java
+++ src/main/java/com/paga/librarycatalogue/api/CatalogueControllers.java
@@ -3,21 +3,31 @@ package com.paga.librarycatalogue.api;
 import com.paga.librarycatalogue.model.Catalogue;
 import com.paga.librarycatalogue.service.CatalogueService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * CatalogueControllers
  */
+@RequestMapping("api/v1/catalogue")
+@RestController
 public class CatalogueControllers {
 
     // Get reference to the catalogue service object
     private final CatalogueService catalogueService;
 
     // Initialize catalogue service object inside the constructor.
+    @Autowired
     public CatalogueControllers(CatalogueService catalogueService) {
         this.catalogueService = catalogueService;
     }
 
     // Method that adds a catalogue
-    public void addCatalogue(Catalogue catalogue) {
+    @PostMapping
+    public void addCatalogue(@RequestBody Catalogue catalogue) {
         catalogueService.addCatalogue(catalogue);
 
     }
